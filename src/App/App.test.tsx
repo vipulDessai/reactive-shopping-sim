f8713feb63f4f1268eea3daa5bc05d1485e45a22
Vipul Dessai <badgamerbad@gmail.com>
2021-01-26 01:52:53 +0530

    init

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import { App } from '@/App';
-
 import { store } from '@/_helpers';
+import { Catalog } from '@/_components';
+import { rootReducer } from '@/_reducers';
 
-describe('render', () => {
-  test('renders landing page', () => {
+describe('Render', () => {
+  test('loading static text', () => {
     render(
       <Provider store={store}>
         <App />
@@ -18,5 +21,34 @@ describe('render', () => {
         name: /reactive shop/i
       })
     ).toHaveTextContent('Reactive Shop');
+  });
+
+  test('loading product tile', async () => {
+    const initialState: Object = {
+      catalog: {
+        products: [
+          {
+            "id": 1,
+            "name": "Product 1",
+            "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+            "price": "100.00",
+            "quantity": 10,
+            "cartQuantity": 10,
+            "image": "http://truth-events.com/wp-content/uploads/2019/09/dummy.jpg"
+          },
+        ],
+        error: null
+      }
+    }
+    const testStore = createStore(rootReducer, initialState);
+
+    render(
+      <Provider store={testStore}>
+        <Catalog />
+      </Provider>
+    );
+
+    const productTitle = await screen.findAllByText(/Product 1/i);
+    expect(productTitle.length).toBe(1);
   });
 });
